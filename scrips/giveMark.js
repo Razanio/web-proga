92c7689a8a473a6334516c88f829deee1a2d9fb0
Yahor Sviarchkou <sverchyp@gmail.com>
2021-12-31 08:11:22 +0300

    fixed

--- scrips/giveMark.js
+++
@@ -1,7 +0,0 @@
-const modalWindow = document.querySelector(".modal-window__mark");
-function openModal(){
-    modalWindow.style.display = 'block';
-}
-function closeModal(){
-    modalWindow.style.display = 'none';
-}
